b335d737b4ae790b272d6d11d5ce75e0483da265
a.karpach <a.karpach@itransition.com>
2020-01-20 19:37:59 +0300

    commit 20.01.2020

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Comments = () => {
-    return(
-        <div>
-            Нет новостей- комментировать нечего.
-        </div>
-    );
+    this.props.data.map((item,index)=>{
+        return(
+            <div>
+                <p className={'news__author'}>{item.author}</p>
+                <p className={'news__text'}>{item.text}</p>
+            </div>
+        );
+    });
 }
+
 
 export default Comments;
